6eda8761ade8f07ec099b982cee2564a92b95ef9
pqz_vscode <peiqz@mail.ustc.edu.cn>
2020-12-03 00:23:14 +0800

    add codes

--- exp1/src/build.py
+++ exp1/src/build.py
@@ -14,13 +14,15 @@ stop_words_table = stopwords.words('english')  # 创建英文默认倒排表
 file_No_dict = []
 inverted_table = {}
 i = 0
-N = 3034  # 文档总数
+#N = 3034  # 文档总数
+#N = 9
+N = 517401
 N_item = 1000
 path0 = "E:\Web_lab\exp1\\"  # 基础目录
 tf_idf_matrix = np.zeros((N_item, N))
 list_df = []
 
-
+#@jit(nopython=True)
 def build_stop_words_table():
     # print(stop_words_table)
     # 扩展英文倒排表
@@ -31,6 +33,7 @@ def build_stop_words_table():
 
 
 # 利用正则表达式过滤除了主题和正文之外的部分
+#@jit(nopython=True)
 def filter(line):
     line = re.sub(r'Message-ID.*$', "", line)
     line = re.sub(r'Date.*$', "", line)
@@ -48,6 +51,7 @@ def filter(line):
 # value[1]字典，为含有该词项的文档（去重）以及该词项在文档中出现的次数
 
 
+#@jit(nopython=True)
 def add_inverted_table(word, freq):
     if word not in inverted_table:
         temp = [1]
@@ -55,61 +59,82 @@ def add_inverted_table(word, freq):
         inverted_table[word] = temp
     else:
         inverted_table[word][0] += 1
-        if [i, freq] not in inverted_table[word]:
-            inverted_table[word].append([i, freq])
+        #if [i, freq] not in inverted_table[word]:
+        inverted_table[word].append([i, freq])
 
 
 # 根据传入的path处理对应的文件，包含预处理和建立倒排表
+#@jit(nopython=True)
 def build_invert_list(path):
     global i
     file_No_dict.append(path)  # 建立映射表
-
-    f = open(path)
-    iter_f = iter(f)
     str = ""
-    for line in iter_f:  # 遍历文件，按行遍历，读取文本，每个文本对应一个字符串
-        line = filter(line)  # 只读取主题和正文
-        str = str + line
-    f.close()
+    try:
+        f = open(path, 'r')
+        iter_f = iter(f)
+        for line in iter_f:  # 遍历文件，按行遍历，读取文本，每个文本对应一个字符串
+            line = filter(line)  # 只读取主题和正文
+            str = str + line
+    except:
+        f.close()
+        try:
+            f = open(path, 'r', encoding='unicode_escape')
+            iter_f = iter(f)
+            for line in iter_f:  # 遍历文件，按行遍历，读取文本，每个文本对应一个字符串
+                line = filter(line)  # 只读取主题和正文
+                str = str + line
+        except:
+            f.close()
+            return 0
+
     text = nltk.word_tokenize(str.lower())  # 分词，并转化为小写，方便通过停用词表去除
     text1 = []
     for word in text:
         w = porter_stemmer.stem(word)  # 词根化处理
         text1.append(w)
     fdist = nltk.FreqDist(text1)
+    # print(text1)
     for word in text1:
         # w = porter_stemmer.stem(word)  # 词根化处理
         if word not in stop_words_table:  # 去停用词处理
             add_inverted_table(word, fdist[word])
     i = i + 1
+    if i % 100 == 0:
+        print(i)
 
 
-def file_read():
-    path = path0 + "data_temp\maildir"  # 数据集目录
-    files_maildir = os.listdir(path)  # 得到文件夹下的所有文件名称
-    for file_person in files_maildir:  # 遍历第一层文件夹maildir
-        path1 = path + "\\" + file_person
-        diff_doc = os.listdir(path1)
-        for doc in diff_doc:       # 遍历第二层文件夹
-            path2 = path1 + "\\" + doc
-            if not os.path.isdir(path2):  # 注意，该层目录中也可能有文件
-                build_invert_list(path2)
-                continue
-            diff_file = os.listdir(path2)
-            for file in diff_file:  # 遍历当前文件夹中的每个文件
-                path3 = path2 + "\\" + file
-                build_invert_list(path3)
+#@jit(nopython=True)
+def file_read1(path):  # path = path0 + "dataset\maildir"
+    folders = os.listdir(path)  #列出路径下所有文件和文件夹
+    if len(folders) == 0:   #如果当前路径下没有文件和文件夹，返回
+        return
+    else:
+        for folder in folders:
+            path1 = path + "\\" + folder
+            if os.path.isfile(path1):   #如果是文件，则加入倒排表
+                build_invert_list(path1)
+            else:
+                file_read1(path1)   #如果是文件夹，则递归调用
+    return 
 
-
+    
+#@jit(nopython=True)
 def write_1000_inverted_table():
     list1 = sorted(inverted_table.items(),
                    key=lambda item: item[1][0], reverse=True)
+    #print(list1)
     path_inverted_table = path0 + "output/inverted_table.txt"
     file_inverted_table = open(path_inverted_table, 'w+')
-    for j in range(N_item):  # 后续改为1000
+    for j in range(N_item):  
         file_inverted_table.write(list1[j][0] + ' : ')
-        for k in range(1, len(list1[j][1])):
-            file_inverted_table.write(str(list1[j][1][k][0]) + ' ')
+        new_list = list1[j][1]
+        del (list1[j][1][0])
+        new_list = de_repetition(new_list)
+        #print(new_list)
+        for k in range(0, len(new_list)):
+            file_inverted_table.write(str(new_list[k][0]) + ' ')
+        #for k in range(1, len(list1[j][1])):  
+            #file_inverted_table.write(str(list1[j][1][k][0]) + ' ')
         file_inverted_table.write('\n')
     # print(list1[0])
     file_inverted_table.close()
@@ -117,25 +142,41 @@ def write_1000_inverted_table():
 
 # 根据传入的list_sorted建立ti_idf矩阵
 
+def de_repetition(old_list):
+    new_list = []
+    for i in old_list:
+        if i not in new_list:
+            new_list.append(i)
+    #print(new_list)
+    return new_list
 
+
+#@jit(nopython=True)
 def tf_idf_matrix_build(list_sorted):
+    #print(list_sorted)
+    new_list = []
+    for row in range(N_item):
+        del (list_sorted[row][1][0])
+        new_list.append(de_repetition(list_sorted[row][1]))
+    #print(new_list)
     for row in range(N_item):  # 后续改为1000
-        list_df.append(len(list_sorted[row][1]) - 1)
-        for col in range(1, len(list_sorted[row][1])):
-            if list_sorted[row][1][col][1] == 0:
-                continue
-            else:
-                tf_idf_matrix[row][list_sorted[row][1][col][0]] = (
-                    1 + math.log10(list_sorted[row][1][col][1])) * math.log10(N / list_df[row])
-    # print(tf_idf_matrix)
-    # print(list_df)
+        list_df.append(len(new_list[row]))
+        for col in range(0, len(list_sorted[row][1]) - 1):
+            tf_idf_matrix[row][list_sorted[row][1][col][0]] = (
+                1 + math.log10(list_sorted[row][1][col][1])) * math.log10(N / list_df[row])
+            # if list_sorted[row][1][col][1] == 0:
+            # continue
+            # else:
+            #tf_idf_matrix[row][list_sorted[row][1][col][0]] = (1 + math.log10(list_sorted[row][1][col][1])) * math.log10(N / list_df[row])
+    #print(tf_idf_matrix)
+    #print(list_df)
     path_tf_idf_matrix = path0 + "output/tf_idf_matrix.npz"
     compressed_matrix = sparse.csr_matrix(tf_idf_matrix)
     sparse.save_npz(path_tf_idf_matrix, compressed_matrix)
 
+
+#@jit(nopython=True)
 # 建立文件编号到路径的映射表
-
-
 def file_No_dict_write():
     path_tf_idf_dictionary = path0 + "output/dictionary.txt"
     dictionary = open(path_tf_idf_dictionary, 'w+')
@@ -146,7 +187,9 @@ def file_No_dict_write():
 
 def main():
     build_stop_words_table()
-    file_read()
+    path_read = path0 + "dataset\maildir"
+    #file_read()
+    file_read1(path_read)
     list_sorted = write_1000_inverted_table()
     tf_idf_matrix_build(list_sorted)
     file_No_dict_write()
--- exp1/src/semantic_search.py
+++ exp1/src/semantic_search.py
@@ -1,27 +1,63 @@
 import numpy as np
 import nltk
 import re
+import math
 from scipy import sparse
 from nltk.corpus import stopwords
 from nltk.stem.porter import PorterStemmer
-
+#np.set_printoptions(threshold=np.inf)  # 保证完整输出矩阵
 porter_stemmer = PorterStemmer()
 
 path0 = "E:\Web_lab\exp1\\"  # 基础目录
-N = 1000
+N = 3034
+N_item = 1000
 word_list = []
+stop_words_table = stopwords.words('english')  # 创建英文默认倒排表
 
-def compare(input_word_vec, tf_idf_col_vec):
-    return
+def build_stop_words_table():
+    # print(stop_words_table)
+    # 扩展英文倒排表
+    for w in ['!', ',', '.', '?', ':', ';', '<', '>', '@', '[', ']', '(', ')', '-', '\'\'', '--', '*', '$',
+              '...', '=', '\'s', '\'t', '|', '%', '..', '&', '#', '`', '``',
+              'subject', 'enron', 'thi', 'need', 'forward', 'would']:
+        stop_words_table.append(w)
+
+
+def tf_idf_normalize(tf_idf_matrix):
+    normalize_matrix = np.zeros((N_item, N))
+    for col in range(N):  # 计算归一化矩阵
+        sum_2 = math.sqrt(np.sum(tf_idf_matrix[:, col] ** 2))
+        #tf_idf_matrix[:, col] = tf_idf_matrix[:, col] / sum_2
+        if sum_2 != 0:
+            normalize_matrix[:, col] = tf_idf_matrix[:, col] / sum_2
+    #print(normalize_matrix)
+    return normalize_matrix
+
+def compare(input_word_vec, normalize_matrix):
+    similarity = {}
+    for col in range(N):
+        #print(normalize_matrix[:, col])
+        #similarity.append(np.inner(normalize_matrix[:, col], input_word_vec))
+        similarity[str(col + 1)] = np.inner(normalize_matrix[:, col], input_word_vec)
+    #print(similarity)
+    result = sorted(similarity.items(), key=lambda item: item[1], reverse=True)[0:10]
+    return result
 
 #计算查询词或词组的向量，不用作归一化（归一化后，在最后比较时只是因子不同）
 def cal_input_query(query_str):
-    vec = np.zeros(1000)
-    query_list = query_str.split()
-    for word in query_list:
+    vec = np.zeros(N_item)  #创建零向量
+    text = nltk.word_tokenize(query_str.lower())
+    text1 = []
+    for word in text:
         w = porter_stemmer.stem(word)  # 词根化处理
-        if w in word_list:
-            vec[word_list.index(w)] = 1
+        text1.append(word)
+    #print(text1)
+    #print(word_list)
+    for word in text1:
+        if word not in stop_words_table:
+            if w in word_list:
+                vec[word_list.index(w)] = 1
+    #print(vec)
     return vec
 
 def main():
@@ -29,16 +65,16 @@ def main():
     path_query = path0 + "input\\semantic_search.txt"
     path_inverted_table = path0 + "output\\inverted_table.txt"
     sparse_matrix = sparse.load_npz(path_npz)
-    tf_idf_matrix = sparse_matrix.todense()
-    # print(tf_idf_matrix)
+    tf_idf_matrix = sparse_matrix.toarray()
+    #print(tf_idf_matrix)
 
     file_query = open(path_query)
     iter_f = iter(file_query)
-    query = []  #存放输入的查询词或词组
+    query_str = ""  #存放输入的查询词或词组
     for line in iter_f:  # 遍历文件，按行遍历，读取文本，每个文本对应一个字符串
         line = re.sub(r'\n.*$', "", line)
         #w = porter_stemmer.stem(line)  # 词根化处理
-        query.append(line)
+        query_str = query_str + line
     file_query.close()
 
     file_inverted_table = open(path_inverted_table)
@@ -47,13 +83,27 @@ def main():
         line = re.sub(r'\n.*$', "", line)
         word_list.append(line.split()[0])
     file_inverted_table.close()
-    #print(query)
-    #print(word_list)
-    #print(len(word_list))
+
+    normalize_matrix = tf_idf_normalize(tf_idf_matrix)
+    #print(query_str)
+
+    vec = cal_input_query(query_str)
+    if np.sum(vec) == 0:
+        print("can not find")
+    else:
+        result = compare(vec, normalize_matrix)
+        print(result)
+'''
     for word in query:
         vec = cal_input_query(word)
-        compare(vec, )
-
+        #zero_vec = np.zeros(N)
+        if np.sum(vec) == 0:
+            print("can not find")
+        else:
+            #print("vec:", vec)
+            result = compare(vec, normalize_matrix)
+            print(result)
+'''
 
 
 if __name__ == "__main__":
